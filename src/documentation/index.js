dd37db6b26bc389b076147db4c057900ae75b42f
Raphael DDL Oliveira <raphael.s.oliveira@bloomingdales.com>
2019-12-30 17:15:01 -0300

    minor pack version bump

--- src/documentation/index.js
+++ src/documentation/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import timeout from 'connect-timeout';
 import Limiter from '../utils/Limiter';
-import { VERSION } from '../utils/Constants';
 import swaggerUi from 'swagger-ui-express';
 import fs from 'fs';
 import yaml from 'js-yaml';
@@ -17,10 +16,10 @@ try {
 	swaggerDocument = {
 		openapi: '3.0.2',
 		info: {
-			title: 'EpicSevenDB.com API v' + VERSION,
+			title: 'EpicSevenDB.com API',
 			description:
 				'Documentation for the E7DB API. If you are seeing this message instead of a proper endpoint documentation, it means Swagger failed to read the documentation file. Please open an [Issue on Github](https://github.com/EpicSevenDB/api/issues) so I can fix this.',
-			version: VERSION || '2.?.?',
+			version: '2.?.?',
 		},
 	};
 }
