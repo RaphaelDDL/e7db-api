20afbff2a34b2acf21b4895e729becfca321e0d9
Raphael DDL Oliveira <ddl@raphaelddl.com>
2020-03-13 19:15:48 -0300

    populating zodiac tree costs items

--- src/router/hero/single.js
+++ src/router/hero/single.js
@@ -24,47 +24,47 @@ export default asyncRoute(async (req, res, next) => {
 			.aggregate([
 				{ $match: { _id } },
 				// converting buff/debuff/other into their data
-				{
-					$lookup: {
-						from: `buffs-${requestedLanguage}`,
-						localField: 'buffs',
-						foreignField: '_id',
-						as: 'buffs',
-					},
-				},
-				{
-					$lookup: {
-						from: `buffs-${requestedLanguage}`,
-						localField: 'debuffs',
-						foreignField: '_id',
-						as: 'debuffs',
-					},
-				},
-				{
-					$lookup: {
-						from: `buffs-${requestedLanguage}`,
-						localField: 'common',
-						foreignField: '_id',
-						as: 'common',
-					},
-				},
+				// {
+				// 	$lookup: {
+				// 		from: `buffs-${requestedLanguage}`,
+				// 		localField: 'buffs',
+				// 		foreignField: '_id',
+				// 		as: 'buffs',
+				// 	},
+				// },
+				// {
+				// 	$lookup: {
+				// 		from: `buffs-${requestedLanguage}`,
+				// 		localField: 'debuffs',
+				// 		foreignField: '_id',
+				// 		as: 'debuffs',
+				// 	},
+				// },
+				// {
+				// 	$lookup: {
+				// 		from: `buffs-${requestedLanguage}`,
+				// 		localField: 'common',
+				// 		foreignField: '_id',
+				// 		as: 'common',
+				// 	},
+				// },
 				// converting zodiac_tree/skills enhancements into their data
-				{
-					$lookup: {
-						from: `materials-${requestedLanguage}`,
-						localField: 'zodiac_tree.costs.item',
-						foreignField: 'identifier',
-						as: 'zodiac_costs_items',
-					},
-				},
-				{
-					$lookup: {
-						from: `materials-${requestedLanguage}`,
-						localField: 'skills.enhancements.costs.item',
-						foreignField: 'identifier',
-						as: 'sk_enhancements_costs_items',
-					},
-				},
+				// {
+				// 	$lookup: {
+				// 		from: `materials-${requestedLanguage}`,
+				// 		localField: 'zodiac_tree.costs.item',
+				// 		foreignField: 'identifier',
+				// 		as: 'zodiac_costs_items',
+				// 	},
+				// },
+				// {
+				// 	$lookup: {
+				// 		from: `materials-${requestedLanguage}`,
+				// 		localField: 'skills.enhancements.costs.item',
+				// 		foreignField: 'identifier',
+				// 		as: 'sk_enhancements_costs_items',
+				// 	},
+				// },
 				// adding ex_equip data if available
 				{
 					$lookup: {
@@ -72,6 +72,75 @@ export default asyncRoute(async (req, res, next) => {
 						localField: 'id',
 						foreignField: 'unit',
 						as: 'ex_equip',
+					},
+				},
+
+				{
+					$unwind: '$zodiac_tree',
+				},
+				{
+					$unwind: '$zodiac_tree.costs',
+				},
+				{
+					$lookup: {
+						from: `materials-${requestedLanguage}`,
+						localField: 'zodiac_tree.costs.item',
+						foreignField: '_id',
+						as: 'zodiac_costs_items',
+					},
+				},
+				{
+					$group: {
+						_id: '$zodiac_tree.name',
+						root: {
+							$first: '$$ROOT',
+						},
+						items: {
+							$push: {
+								$mergeObjects: [
+									'$zodiac_tree.costs',
+									{
+										$arrayElemAt: ['$zodiac_costs_items', 0],
+									},
+								],
+							},
+						},
+					},
+				},
+				{
+					$addFields: {
+						'root.zodiac_tree.costs': '$items',
+					},
+				},
+				{
+					$replaceRoot: {
+						newRoot: '$root',
+					},
+				},
+				{
+					$group: {
+						_id: '$_id',
+						root: {
+							$first: '$$ROOT',
+						},
+						slots: {
+							$push: '$costs',
+						},
+					},
+				},
+				{
+					$addFields: {
+						'root.zodiac_tree': '$zodiac_tree',
+					},
+				},
+				{
+					$replaceRoot: {
+						newRoot: '$root',
+					},
+				},
+				{
+					$project: {
+						zodiac_costs_items: 0,
 					},
 				},
 			])
